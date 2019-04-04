45beb35d51a506c9628ef9570c5355ec6906fdc6
lukdru <30797583+lukdru@users.noreply.github.com>
2019-04-04 18:10:30 +0300

    04/04

---
+++ KTU_VR0.5/Assets/Scripts/VR_Main.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VR_Main : MonoBehaviour
+{
+
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabAction;
+    public SteamVR_Action_Boolean teleportAction;
+
+    void Update()
+    {
+        if(GetTeleport())
+        {
+            print("Teleport " + handType);
+        }
+
+        if (GetGrab())
+        {
+            print("Grab " + handType);
+        }
+    }
+
+    public bool GetTeleport()
+    {
+        return teleportAction.GetStateDown(handType);
+    }
+
+    public bool GetGrab()
+    {
+        return grabAction.GetState(handType);
+    }
+}
