0789a4bccf603c26eb67e5019dde1963996b3f33
lukdru <30797583+lukdru@users.noreply.github.com>
2019-04-10 17:46:32 +0300

    04/10

---
+++ KTU_VR0.5/Assets/Scripts/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int id;
+    public string title;
+    public string description;
+
+    public Item(int id, string title, string description)
+    {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public void Update()
+    {
+    }
+
+
+}
---
+++ KTU_VR0.5/Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public GameObject ControllerRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ControllerRight.transform.eulerAngles.z > 145 && ControllerRight.transform.eulerAngles.z < 155 && ControllerRight.transform.eulerAngles.x < 150)
+            Debug.Log("Controller Inverted");
+        else
+            Debug.Log(ControllerRight.transform.eulerAngles.z);
+    }
+}
