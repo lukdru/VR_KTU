0c95fbd153185a6d0cb368a25929fd0d0ed9b057
lukdru <30797583+lukdru@users.noreply.github.com>
2019-04-16 19:03:04 +0300

    04-16

--- KTU_VR0.5/Assets/Scripts/Main.cs
+++ KTU_VR0.5/Assets/Scripts/Main.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     public GameObject ControllerRight;
-    public Camera
+    public Camera MainCamera;
+
+    public int Points;
 
     // Update is called once per frame
     void Update()
+    {
+        //Check();
+        Check2();
+    }
+
+    public void Check()
     {
         if (ControllerRight.transform.eulerAngles.z > 145 && ControllerRight.transform.eulerAngles.z < 155 && ControllerRight.transform.eulerAngles.x < 150)
             Debug.Log("Controller Inverted");
         else
             Debug.Log(ControllerRight.transform.eulerAngles.z);
+    }
+
+    public void Check2()
+    {
+        if (ControllerRight.transform.forward == MainCamera.transform.forward)
+        {
+            Debug.Log("Controller Inverted");
+        }
     }
 }
---
+++ KTU_VR0.5/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public int pointsToAdd;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            ScoreManager.AddPoints(pointsToAdd);
+        }
+        Destroy(gameObject);
+    }
+      
+}
