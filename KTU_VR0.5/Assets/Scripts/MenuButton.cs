3eae0a50b4510b6b7ae73f4457dc8b927aa55943
lukdru <30797583+lukdru@users.noreply.github.com>
2019-05-01 21:34:23 +0300

    05/01

---
+++ KTU_VR0.5/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    private string levelLoad = "MainScene";
+
+    void onTriggerEnter(Collider other)
+    {
+        Debug.Log("Collide");
+        if (other.gameObject.tag =="Player")
+        {
+            Debug.Log("Kita Scena");
+            SceneManager.LoadScene(levelLoad);
+        }
+    }
+}
