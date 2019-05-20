32c65af960a273fe9e36b0d6a82063b1042147cf
lukdru <30797583+lukdru@users.noreply.github.com>
2019-05-20 20:17:17 +0300

    05/20

---
+++ KTU_VR0.5/Assets/Scripts/BasketballCourt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketballCourt : MonoBehaviour
+{
+
+    public GameObject Ball;
+    public GameObject SpawnPlace;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            Debug.Log("Collidino su ribomis!!!!!!!");
+           //Ball.GetComponent<Rigidbody>().isKinematic = true;
+            //Ball.GetComponent<Rigidbody>().isKinematic = false;
+            //Ball.transform.position = SpawnPlace.transform.position;
+
+            Instantiate(Ball, SpawnPlace.transform.position, Quaternion.identity);
+        }
+    }
+
+
+
+
+}
